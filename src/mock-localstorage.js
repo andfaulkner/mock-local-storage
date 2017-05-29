e20a240ae5e9ff224e60f5b63d1b5ffb8ed0a9de
Andrew Faulkner <andfaulkner@gmail.com>
2017-05-29 09:05:33 -0400

    Now works in newer Node versions. Rm side-effects; TS-friendliness; object export.
    Runs in Node 6.*.* series (previously it was not working).
    
    Removed all implicit, unrequested "on-import" side-effects - made global binding optional instead (it now requires a call to bindBrowserStorageGloball).
    
    Wrapped all parts in functions.
    
    Made Typescript-friendly: typings now work with intellisense, and can also be explicitly imported via the BrowserStorage type.
    
    Etc.

--- src/mock-localstorage.js
+++ src/mock-localstorage.js
@@ -1,80 +1,116 @@
-// Mock localStorage 
-(function (glob) {
+//
+// Loosely based on https://github.com/letsrock-today/mock-local-storage
+// Thanks letsrock-today!
+//
 
-    function createStorage() {
-        let s = {},
-            noopCallback = () => {},
-            _itemInsertionCallback = noopCallback;
+/**
+ * Construct a mock Storage (localStorage / sessionStorage) object. Should be
+ * bound to a global, to act as a shim for browser localStorage or sessionStorage
+ * in a NodeJS environment.
+ *
+ * @return {Storage} New 'Storage' object
+ * @example window.localStorage = mockBrowserStorage();
+ */
+export const mockBrowserStorage = function mockBrowserStorage() {
+    let store = {} as Storage;
+    const noopCallback = (...args: any[]) => {};
+    let _itemInsertionCallback: Function = noopCallback;
 
-        Object.defineProperty(s, 'setItem', {
-            get: () => {
-                return (k, v) => {
-                    k = k + '';
-                    if (!s.hasOwnProperty(k)) {
-                        _itemInsertionCallback(s.length);
-                    }
-                    s[k] = v + '';
-                };
+    store.setItem = (k: string, val: string) => {
+        k = k + '';
+        if (!store.hasOwnProperty(k)) {
+            _itemInsertionCallback(store.length);
+        }
+        store[k] = val + '';
+    };
+
+
+    store.getItem = (k: string): string => {
+        k = k + '';
+        if (store.hasOwnProperty(k)) {
+            return store[k];
+        } else {
+            return null;
+        }
+    };
+
+    store.removeItem = (k: string): void => {
+        k = k + '';
+        if (store.hasOwnProperty(k)) {
+            delete store[k];
+        }
+    };
+
+    store.clear = (): void => {
+        for (let k in store) {
+            if (store.hasOwnProperty(k)) {
+                delete store[k];
             }
-        });
-        Object.defineProperty(s, 'getItem', {
-            get: () => {
-                return k => {
-                    k = k + '';
-                    if (s.hasOwnProperty(k)) {
-                        return s[k];
-                    } else {
-                        return null;
-                    }
-                };
+        }
+    };
+
+    Object.defineProperty(store, 'length', {
+        enumerable: true,
+        value: () => {
+            return Object.keys(store).length;
+        },
+    });
+
+    Object.defineProperty(store, 'key', {
+        enumerable: true,
+        value: (k: string) => {
+            let key = Object.keys(store)[k];
+            return (!key) ? null : key;
+        },
+    });
+
+    Object.defineProperty(store, 'itemInsertionCallback', {
+        get: () => {
+            return _itemInsertionCallback;
+        },
+        set: (val: Function) => {
+            if (!val || typeof val != 'function') {
+                val = noopCallback;
             }
+            _itemInsertionCallback = val;
+        }
+    });
+
+    return store;
+};
+
+// NOTE: this is a bit evil - instantiating just to get typings. This is due to
+// a Typescript limitation, sadly.
+const baseMockBrowserStorage = mockBrowserStorage();
+export type BrowserStorage = typeof baseMockBrowserStorage;
+
+// Typings to make bindBrowserStorageGlobally happy.
+declare const global: NodeJS.Global & {window: { localStorage: Storage, sessionStorage: Storage }};
+declare const window: Window;
+
+/**
+ * Attempt to automatically bind localStorage and sessionStorage globally.
+ * @return {boolean} true if it succeeds, false if it doesn't.
+ */
+export const bindBrowserStorageGlobally = () => {
+    if (typeof global !== 'undefined') {
+        if (typeof global.window === 'undefined') {
+            global.window = {
+                localStorage: mockBrowserStorage(),
+                sessionStorage: mockBrowserStorage(),
+            };
+        } else {
+            global.window.localStorage = mockBrowserStorage();
+            global.window.sessionStorage = mockBrowserStorage();
+        }
+        return true;
+
+    } else if (typeof window !== 'undefined') {
+        Object.assign(window, {
+            localStorage: mockBrowserStorage(),
+            sessionStorage: mockBrowserStorage(),
         });
-        Object.defineProperty(s, 'removeItem', {
-            get: () => {
-                return k => {
-                    k = k + '';
-                    if (s.hasOwnProperty(k)) {
-                        delete s[k];
-                    }
-                };
-            }
-        });
-        Object.defineProperty(s, 'clear', {
-            get: () => {
-                return () => {
-                    for (let k in s) {
-                        if (s.hasOwnProperty(k)) {
-                            delete s[k];
-                        }
-                    }
-                };
-            }
-        });
-        Object.defineProperty(s, 'length', {
-            get: () => {
-                return Object.keys(s).length;
-            }
-        });
-        Object.defineProperty(s, "key", {
-            value: k => {
-                let key = Object.keys(s)[k];
-                return (!key) ? null : key;
-            },
-        });
-        Object.defineProperty(s, 'itemInsertionCallback', {
-            get: () => {
-                return _itemInsertionCallback;
-            },
-            set: v => {
-                if (!v || typeof v != 'function') {
-                    v = noopCallback;
-                }
-                _itemInsertionCallback = v;
-            }
-        });
-        return s;
+        return true;
     }
-
-    glob.localStorage = createStorage();
-    glob.sessionStorage = createStorage();
-}(typeof window !== 'undefined' ? window : global));
+    return false;
+};
